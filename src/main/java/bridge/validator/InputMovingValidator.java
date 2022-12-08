87642946d8afcb665787e65e7ed5485d3bebd3ac
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-12-08 15:54:24 +0900

    refactor : 예외 검사 순서 변경

--- src/main/java/bridge/validator/InputMovingValidator.java
+++ src/main/java/bridge/validator/InputMovingValidator.java
@@ -5,14 +5,14 @@ import bridge.view.ErrorMessage;
 public class InputMovingValidator {
 
     public static void checkInputMove(String inputMove) {
-        checkCommand(inputMove);
         InputCommonValidator.checkInputLength(inputMove);
-        InputCommonValidator.checkLowerCase(inputMove);
         InputCommonValidator.checkNull(inputMove);
+        InputCommonValidator.checkLowerCase(inputMove);
+        checkCommand(inputMove);
     }
 
     private static void checkCommand(String inputMove) {
-        if (!inputMove.equals("U") || !inputMove.equals("D")) {
+        if (!inputMove.equals("U") && !inputMove.equals("D")) {
             throw new IllegalArgumentException(ErrorMessage.MOVE_COMMAND.getError());
         }
     }
