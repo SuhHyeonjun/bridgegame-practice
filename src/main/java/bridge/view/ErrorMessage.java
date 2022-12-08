9a54a19ac2e4619b9b2801d9086ae5f76012f3ca
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-12-08 15:56:44 +0900

    feat : 이동할 다리 입력 시 발생하는 예외 메시지 추가

--- src/main/java/bridge/view/ErrorMessage.java
+++ src/main/java/bridge/view/ErrorMessage.java
@@ -1,9 +1,12 @@
 package bridge.view;
 
 public enum ErrorMessage {
-    ERROR_BRIDGE_RANGE("[ERROR] 3 이상 20 이하의 숫자를 입력해야 합니다."),
-    ERROR_BLANK("[ERROR] 아무런 값을 입력하지 않았습니다."),
-    ERROR_INPUT_TYPE("[ERROR] 문자를 입력할 수 없습니다.");
+    BRIDGE_RANGE("[ERROR] 3 이상 20 이하의 숫자를 입력해야 합니다."),
+    INPUT_BLANK("[ERROR] 아무런 값을 입력하지 않았습니다."),
+    INPUT_TYPE("[ERROR] 문자를 입력할 수 없습니다."),
+    MOVE_COMMAND("[ERROR] U 또는 D를 입력해야 합니다."),
+    COMMAND_LENGTH("[ERROR] U 또는 D 중, 하나의 문자만 입력할 수 있습니다."),
+    COMMAND_LOWER("[ERROR] 대문자로 입력해야 합니다.");
 
     private final String error;
 
