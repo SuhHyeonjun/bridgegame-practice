86a72ea6ef9210564da05732f418710e12161585
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-12-08 15:25:02 +0900

    feat : 빈 값 입력 시 예외 처리 추가

--- src/main/java/bridge/validator/InputCommonValidator.java
+++ src/main/java/bridge/validator/InputCommonValidator.java
@@ -4,11 +4,7 @@ import bridge.view.ErrorMessage;
 
 public class InputCommonValidator {
 
-    public static void checkCommonError(String input) {
-
-    }
-
-    private static void checkNull(String input) {
+    public static void checkNull(String input) {
         if (input.length() == 0) {
             throw new IllegalArgumentException(ErrorMessage.ERROR_BLANK.getError());
         }
