e7e22cf291eb6196ce4eb0e4de56c6eb98a6067e
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-12-08 15:56:07 +0900

    chore : 변경된 에러 메시지 값에 맞게 변경

--- src/main/java/bridge/validator/InputSizeValidator.java
+++ src/main/java/bridge/validator/InputSizeValidator.java
@@ -13,14 +13,14 @@ public class InputSizeValidator {
     private static void checkSizeRange(String inputSize) {
         int size = Integer.parseInt(inputSize);
         if (size < 3 || size > 20) {
-            throw new IllegalArgumentException(ErrorMessage.ERROR_BRIDGE_RANGE.getError());
+            throw new IllegalArgumentException(ErrorMessage.BRIDGE_RANGE.getError());
         }
     }
 
     private static void checkType(String inputSize) {
         String regex = "^[0-9]*$";
         if (!inputSize.matches(regex)) {
-            throw new IllegalArgumentException(ErrorMessage.ERROR_INPUT_TYPE.getError());
+            throw new IllegalArgumentException(ErrorMessage.INPUT_TYPE.getError());
         }
     }
 }
