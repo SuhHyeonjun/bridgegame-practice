7751727510819987947adc21ab088b00961dfb00
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-12-09 14:01:32 +0900

    feat : 현재까지 이동한 다리 상태 출력 기능 추가

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -1,9 +1,18 @@
 package bridge.view;
 
+import static bridge.BridgeToMove.getDownBridge;
+import static bridge.BridgeToMove.getUpBridge;
+
+import java.util.List;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
+    private static final String CENTER_BAR = " | ";
+    private static final String LEFT = "[ ";
+    private static final String RIGHT = " ]";
+
 
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
@@ -11,6 +20,12 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printMap() {
+        System.out.println(LEFT + joinCenterBar(getUpBridge()) + RIGHT);
+        System.out.println(LEFT + joinCenterBar(getDownBridge()) + RIGHT);
+    }
+
+    private String joinCenterBar(List<String> upDownBridges) {
+        return String.join(CENTER_BAR, upDownBridges);
     }
 
     /**
@@ -19,5 +34,6 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printResult() {
+        System.out.println(Message.GAME_RESULT.getMessage());
     }
 }
