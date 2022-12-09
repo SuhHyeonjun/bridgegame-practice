494837abdf607b44a5f707f4867df5ebb1a11bc8
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-12-09 16:57:58 +0900

    fix : 게임 성공 실패 여부 출력 기능 오류 수정

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -13,8 +13,10 @@ public class OutputView {
     private static final String CENTER_BAR = " | ";
     private static final String LEFT = "[ ";
     private static final String RIGHT = " ]";
-    private static final String SUCCESS = "성공";
-    private static final String FAIL = "실패";
+    private static final String GAME_SUCCESS = "성공";
+    private static final String GAME_FAIL = "실패";
+    private static final String SUCCESS = "O";
+    private static final String FAIL = "X";
 
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
@@ -36,20 +38,27 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult(int bridgeSize, int location) {
+    public void printResult() {
         System.out.println();
         System.out.println(Message.GAME_RESULT.getMessage());
         printMap();
-        printSuccessOrFailure(bridgeSize, location);
-        System.out.println(Message.TRY_COUNT.getMessage() + BridgeGame.getCount());
+        printSuccessOrFailure();
+        printTryCount();
     }
 
-    private static void printSuccessOrFailure(int bridgeSize, int location) {
-        if (bridgeSize == location) {
-            System.out.println(Message.SUCCESS_OR_FAILURE.getMessage() + SUCCESS);
+    private static void printSuccessOrFailure() {
+        List<String> upBridges = getUpBridge();
+        List<String> downBridges = getDownBridge();
+        int endBridge = upBridges.size() - 1;
+        if (upBridges.get(endBridge).equals(SUCCESS) || downBridges.get(endBridge).equals(SUCCESS)) {
+            System.out.println(Message.SUCCESS_OR_FAILURE.getMessage() + GAME_SUCCESS);
         }
-        if (bridgeSize != location) {
-            System.out.println(Message.SUCCESS_OR_FAILURE.getMessage() + FAIL);
+        if (upBridges.get(endBridge).equals(FAIL) || downBridges.get(endBridge).equals(FAIL)) {
+            System.out.println(Message.SUCCESS_OR_FAILURE.getMessage() + GAME_FAIL);
         }
+    }
+
+    private static void printTryCount() {
+        System.out.println(Message.TRY_COUNT.getMessage() + BridgeGame.getCount());
     }
 }
