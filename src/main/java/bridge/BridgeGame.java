5e84bb319db7f293ad96cfcfefca7b74ea55f0a7
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-12-09 16:32:54 +0900

    feat : 게임 재시작 메서드 추가

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -8,14 +8,16 @@ import java.util.List;
 public class BridgeGame {
     private static final String SUCCESS = "O";
     private static final String FAIL = "X";
+    private static final String RESTART = "R";
+    private static int gameCount = 1;
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public String move(List<String> bridge, String moveCommand, int location) {
-        if (bridge.get(location).equals(moveCommand)) {
+    public String move(List<String> bridges, String moveCommand, int location) {
+        if (bridges.get(location).equals(moveCommand)) {
             return SUCCESS;
         }
         return FAIL;
@@ -26,6 +28,15 @@ public class BridgeGame {
      * <p>
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void retry() {
+    public boolean retry(String inputRestart) {
+        if (inputRestart.equals(RESTART)) {
+            gameCount++;
+            return true;
+        }
+        return false;
+    }
+
+    public static int getCount() {
+        return gameCount;
     }
 }
