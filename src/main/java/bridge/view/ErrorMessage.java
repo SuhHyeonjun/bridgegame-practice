5433f37100ec6cdef66f757770e3b3b73921b4f0
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-12-09 14:41:01 +0900

    feat : 재시작 입력 예외 메시지 추가

--- src/main/java/bridge/view/ErrorMessage.java
+++ src/main/java/bridge/view/ErrorMessage.java
@@ -5,8 +5,9 @@ public enum ErrorMessage {
     INPUT_BLANK("[ERROR] 아무런 값을 입력하지 않았습니다."),
     INPUT_TYPE("[ERROR] 문자를 입력할 수 없습니다."),
     MOVE_COMMAND("[ERROR] U 또는 D를 입력해야 합니다."),
-    COMMAND_LENGTH("[ERROR] U 또는 D 중, 하나의 문자만 입력할 수 있습니다."),
-    COMMAND_LOWER("[ERROR] 대문자로 입력해야 합니다.");
+    COMMAND_LENGTH("[ERROR] 하나의 문자만 입력할 수 있습니다."),
+    COMMAND_LOWER("[ERROR] 대문자로 입력해야 합니다."),
+    RESTART_COMMAND("[ERROR] R 또는 Q를 입력해야 합니다.");
 
     private final String error;
 
